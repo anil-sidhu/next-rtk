1ffa564610195fc7bbeebd21f2b5133295c01cf1
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-09-05 22:45:42 +0530

    Done with compoennt

---
+++ src/app/components/AddUsers.js
@@ -0,0 +1,7 @@
+export default function AddUsers(){
+    return(<div className="add-user">
+        <h3>User List</h3>
+        <input type="text" className="add-user-input" placeholder="Add New User" />
+        <button className="add-user-btn">Add User</button>
+    </div>)
+}
---
+++ src/app/components/DisplayUsers.js
@@ -0,0 +1,5 @@
+export default function DisplayUsers(){
+    return(<div className="display-user">
+        <h3>User List</h3>
+    </div>)
+}
