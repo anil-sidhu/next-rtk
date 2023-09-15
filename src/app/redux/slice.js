515efee613df483c28678a037ce935b24271e8f6
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-09-15 18:09:58 +0530

    done

--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,29 +1,32 @@
-const { createSlice,nanoid } = require("@reduxjs/toolkit");
+const { createSlice, nanoid, current } = require("@reduxjs/toolkit");
 
-const initialState={
-    users:[]
+const initialState = {
+    users: JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
 }
 const Slice = createSlice({
-    name:"addUserSlice",
+    name: "addUserSlice",
     initialState,
-    reducers:{
-        addUser:(state,action)=>{
-          
-            const data={
-                id:nanoid(),
-                name:action.payload
+    reducers: {
+        addUser: (state, action) => {
+
+            const data = {
+                id: nanoid(),
+                name: action.payload
             }
+
             state.users.push(data);
+            let userData = JSON.stringify(current(state.users));
+            localStorage.setItem("users", userData)
         },
-        removeUser:(state,action)=>{
-            const data= state.users.filter((item)=>{
-                return item.id!==action.payload
+        removeUser: (state, action) => {
+            const data = state.users.filter((item) => {
+                return item.id !== action.payload
             })
-            state.users=data;
-            
+            state.users = data;
+
         }
     }
 });
 
-export const {addUser,removeUser} = Slice.actions;
+export const { addUser, removeUser } = Slice.actions;
 export default Slice.reducer;
